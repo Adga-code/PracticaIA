cacba525043a9502ddc3331da21ea9f0646ea3c5
Adga-code <adga.trabajo@gmail.com>
2025-10-31 15:48:22 -0300

    aaa

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ from src.services import *
 
 functions.s()
 print("Welcome to the Application!")
-
-import src.models.muestra as muestra
-#import src.models.entrenamiento as entrenamiento
+while True:
+    while True:
+        try:
+            print("Select mode: 1 for Training, 2 for Sample, 0 to Exit")
+            opc = int(input("---> "))
+            functions.s()
+            if opc in [1,2, 0]:
+                break
+            else:
+                functions.s()
+                print("Please enter 1 or 2.")
+        except ValueError:
+            functions.s()
+            print("Please enter a valid integer.")
+    if opc == 1:
+        import src.models.entrenamiento as entrenamiento
+    elif opc == 2:
+        import src.models.muestra as muestra
+    else:
+        break
+    functions.s()
--- src/models/entrenamiento.py
+++ src/models/entrenamiento.py
@@ -1,13 +1,27 @@
 import src.database.connect as connect
 from src.services.interpreter import train_model
 from random import randint
+from time import sleep
+
+automatico = False
 
 while True:
     while True:
         try:
             pre = randint(1,5)
             print(pre)
-            res = int(input("---> "))
+            if automatico:
+                res = 6 - pre
+                print(f"---> {res}")
+                sleep(2)
+            else:
+                res = int(input("---> "))
+            if res == 15:
+                print("MODO AUTOMATICO ACTIVADO")
+                sleep(2)
+                automatico = True
+                continue
+            
             if res in [0,1,2,3,4,5]:
                 break
             else:
